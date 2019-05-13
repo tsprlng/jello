6530f567f438bb5a7cc4885d77e3832c05e9c166
Tom Spurling <tom@tspurling.co.uk>
2019-05-13 14:56:37 +0100

    Can run normally now

--- run.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/ruby
-
-# This file is for symlinking to; it will invoke jello.rb with the correct rvm config.
-system('rvm', '2.2@tsprlng-jello', 'do', 'ruby', "#{__dir__}/jello.rb", *ARGV)
